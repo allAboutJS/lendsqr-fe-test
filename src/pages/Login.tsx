4065e53f4e5341f3c486b4ddd8adb37a4bfaa68a
Onah Victor <victoronah.dev@gmail.com>
2024-11-09 11:11:08 +0100

    chore: create login route and page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import Logo from "../components/Logo";
+import bannerImg from "../assets/images/login-banner-img.png";
+import "../styles/Login.scss";
+import LoginForm from "../components/LoginForm";
+
+const Login = () => {
+	return (
+		<div className="login-page">
+			<aside>
+				<div className="logo-container">
+					<Logo />
+				</div>
+				<div className="img-banner">
+					<img
+						src={bannerImg}
+						alt="Animated man walking through door"
+					/>
+				</div>
+			</aside>
+			<main>
+				<div className="logo-container">
+					<Logo width={100} />
+				</div>
+				<div className="login-form-container">
+					<div>
+						<h1 className="bold">Welcome!</h1>
+						<p>Enter details to login.</p>
+					</div>
+					<LoginForm />
+				</div>
+			</main>
+		</div>
+	);
+};
+
+export default Login;
