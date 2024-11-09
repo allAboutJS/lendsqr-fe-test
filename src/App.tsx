a13d5ce83c2029dcaace73c6b442f7bce0d20a53
Onah Victor <victoronah.dev@gmail.com>
2024-11-09 01:01:57 +0100

    chore: update src/App.tsx for client side routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return null
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
 
-export default App
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Login />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
