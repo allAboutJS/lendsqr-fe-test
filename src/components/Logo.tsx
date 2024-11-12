f60b4824842027265a6456447c667867155644c1
Onah Victor <victoronah.dev@gmail.com>
2024-11-12 05:00:39 +0100

    update: add styling to `Logo` component with media break-point

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,7 @@
 import type { ImgHTMLAttributes } from "react";
 import logo from "../assets/images/logo.png";
+import '../styles/components/Logo.scss'
 
-const Logo = ({ width, ...otherProps}: Omit<ImgHTMLAttributes<HTMLImageElement>, 'src' | 'alt'>) => <img width={width || 173.76} src={logo} alt="Company Logo" {...otherProps} />;
+const Logo = ({ width, ...otherProps}: Omit<ImgHTMLAttributes<HTMLImageElement>, 'src' | 'alt'>) => <img className="logo" width={width || 173.76} src={logo} alt="Company Logo" {...otherProps} />;
 
 export default Logo;
