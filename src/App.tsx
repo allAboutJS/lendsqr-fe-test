db6fbc332560a66457e8616508259bac25a7d5ac
Onah Victor <victoronah.dev@gmail.com>
2024-11-14 01:30:28 +0100

    chore: add dashboard paths and layout, and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import "./styles/App.scss";
 import "./styles/NotificationsSystem.scss";
+import RootLayout from "./pages/Dashboard/Layout";
+import IndexPage from "./pages/Dashboard/Users/Index";
+import UserDetails from "./pages/Dashboard/Users/UserDetails";
+import userDetailsLoader from "./utils/userDetailsLoader";
+import NotFound from "./pages/404";
 
 const router = createBrowserRouter([
   {
     index: true,
     element: <Login />,
+  },
+  {
+    path: "/dashboard",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/dashboard/users",
+        element: <IndexPage />,
+      },
+      {
+        path: "/dashboard/users/:id",
+        element: <UserDetails />,
+        loader: userDetailsLoader,
+      },
+      {
+        path: "/dashboard/*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
