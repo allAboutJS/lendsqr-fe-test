cb249e47440562a9ae4f6881aabe62112084fabf
Onah Victor <victoronah.dev@gmail.com>
2024-11-14 21:00:04 +0100

    chore: use star icon ro represent user tier

--- src/pages/Dashboard/Users/UserDetails.tsx
+++ src/pages/Dashboard/Users/UserDetails.tsx
@@ -69,7 +69,30 @@ const UserDetails = () => {
 					<span></span>
 					<div>
 						<p>User's Tier</p>
-						<p>{user.tier}</p>
+						<p>
+							{Array(user.tier).fill(null).map(() => (
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									height="20px"
+									viewBox="0 -960 960 960"
+									width="20px"
+									fill="#D4AF37"
+								>
+									<path d="m243-144 63-266L96-589l276-24 108-251 108 252 276 23-210 179 63 266-237-141-237 141Z" />
+								</svg>
+							))}
+							{Array(3 - user.tier).fill(null).map(() => (
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									height="20px"
+									viewBox="0 -960 960 960"
+									width="20px"
+									fill="#D4AF37"
+								>
+									<path d="m352-293 128-76 129 76-34-144 111-95-147-13-59-137-59 137-147 13 112 95-34 144ZM243-144l63-266L96-589l276-24 108-251 108 252 276 23-210 179 63 266-237-141-237 141Zm237-333Z" />
+								</svg>
+							))}
+						</p>
 					</div>
 					<span></span>
 					<div>
