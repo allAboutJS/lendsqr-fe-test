4d4ee8c1e4a4980256ef7423cc8a58c5521f7db8
Onah Victor <victoronah.dev@gmail.com>
2024-11-14 20:43:36 +0100

    refactor: remove async from promise loader

--- src/utils/fetchUsers.ts
+++ src/utils/fetchUsers.ts
@@ -2,19 +2,18 @@ import { User } from "../../types";
 
 /** Helper function to fetch user from remote server */
 const fetchUsers = (): Promise<User[] | undefined> =>
-    new Promise(async (resolve) => {
-        try {
-            const response = await fetch(
-                "https://run.mocky.io/v3/8b6ba517-9731-492a-a3cc-09ecb86f02b3",
-            );
-            const { data } = await response.json();
-
-            resolve(data as User[]);
-        } catch (error: any) {
-            console.error("Error fetching users", error);
-            // Don't reject. Return undefined instead.
-            resolve(undefined);
-        }
+    new Promise((resolve) => {
+        fetch(
+            "https://run.mocky.io/v3/8b6ba517-9731-492a-a3cc-09ecb86f02b3",
+        )
+            .then((response) => response.json())
+            .then(({ data }) => resolve(data as User[]))
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .catch((error: any) => {
+                console.error("Error fetching users", error);
+                // Don't reject. Return undefined instead.
+                resolve(undefined);
+            });
     });
 
 export default fetchUsers;
