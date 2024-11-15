5421592dc7608681ffc7ba41b2aea355859cb7d5
Onah Victor <victoronah.dev@gmail.com>
2024-11-15 02:08:19 +0100

    update: add dynamic user qualification and residence type

--- src/pages/Dashboard/Users/UserDetails.tsx
+++ src/pages/Dashboard/Users/UserDetails.tsx
@@ -70,28 +70,32 @@ const UserDetails = () => {
 					<div>
 						<p>User's Tier</p>
 						<p>
-							{Array(user.tier).fill(null).map(() => (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#D4AF37"
-								>
-									<path d="m243-144 63-266L96-589l276-24 108-251 108 252 276 23-210 179 63 266-237-141-237 141Z" />
-								</svg>
-							))}
-							{Array(3 - user.tier).fill(null).map(() => (
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="20px"
-									viewBox="0 -960 960 960"
-									width="20px"
-									fill="#D4AF37"
-								>
-									<path d="m352-293 128-76 129 76-34-144 111-95-147-13-59-137-59 137-147 13 112 95-34 144ZM243-144l63-266L96-589l276-24 108-251 108 252 276 23-210 179 63 266-237-141-237 141Zm237-333Z" />
-								</svg>
-							))}
+							{Array(user.tier)
+								.fill(null)
+								.map(() => (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										height="20px"
+										viewBox="0 -960 960 960"
+										width="20px"
+										fill="#D4AF37"
+									>
+										<path d="m243-144 63-266L96-589l276-24 108-251 108 252 276 23-210 179 63 266-237-141-237 141Z" />
+									</svg>
+								))}
+							{Array(3 - user.tier)
+								.fill(null)
+								.map(() => (
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										height="20px"
+										viewBox="0 -960 960 960"
+										width="20px"
+										fill="#D4AF37"
+									>
+										<path d="m352-293 128-76 129 76-34-144 111-95-147-13-59-137-59 137-147 13 112 95-34 144ZM243-144l63-266L96-589l276-24 108-251 108 252 276 23-210 179 63 266-237-141-237 141Zm237-333Z" />
+									</svg>
+								))}
 						</p>
 					</div>
 					<span></span>
@@ -176,7 +180,12 @@ const UserDetails = () => {
 						</div>
 						<div>
 							<div>TYPE OF RESIDENCE</div>
-							<div>{faker.location.streetAddress()}</div>
+							<div>{faker.helpers.arrayElement([
+									"Parent's house",
+									"Hotel apartment",
+									"Estate.",
+									"Personal house",
+								])}</div>
 						</div>
 					</div>
 				</div>
@@ -186,7 +195,18 @@ const UserDetails = () => {
 					<div>
 						<div>
 							<div>LEVEL OF EDUCATION</div>
-							<div>Bsc.</div>
+							<div>
+								{faker.helpers.arrayElement([
+									"LLB",
+									"MBBS",
+									"BSc.",
+									"BA",
+									"OND",
+									"HND",
+									"MSc.",
+									"MA",
+								])}
+							</div>
 						</div>
 						<div>
 							<div>EMPLOYMENT STATUS</div>
